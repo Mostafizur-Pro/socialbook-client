f70a8a9627c329ac72c258f40ffda5848a9cd2da
Mostafizur Rahman <mostafizur0195@gmail.com>
2023-02-01 11:14:47 +0600

    contact page design

--- src/Pages/Contact/ContactPage/ContactPage.js
+++ src/Pages/Contact/ContactPage/ContactPage.js
@@ -14,8 +14,9 @@ const ContactPage = () => {
   return (
     <div>
       <div className="hero  bg-base-200 ">
-        <div className="hero-content flex-col  lg:flex-row-reverse">
-          <section className="text-center lg:text-left">
+        <div className="hero-content flex-col shadow-2xl bg-base-100 lg:flex-row-reverse">
+          <section className=" text-center lg:text-left">
+            {/* <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"></div> */}
             <h1 className="text-2xl font-bold">Reach Us</h1>
             <div className="flex items-center my-3 gap-5">
               <p className="text-green-400 text-xl">
@@ -65,37 +66,43 @@ const ContactPage = () => {
               </a>
             </div>
           </section>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
+          <div>
+            <form className="card-body">
+              <h1 className="text-xl">Leave a Message</h1>
               <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
                 <input
                   type="text"
-                  placeholder="email"
+                  placeholder="Enter your name *"
                   className="input input-bordered"
                 />
               </div>
               <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
                 <input
-                  type="text"
-                  placeholder="password"
+                  type="email"
+                  placeholder="Enter your email *"
                   className="input input-bordered"
                 />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
               </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+              <div className="form-control">
+                <input
+                  type="text"
+                  placeholder="Enter your phone *"
+                  className="input input-bordered"
+                />
               </div>
-            </div>
+              <div className="form-control">
+                <textarea
+                  type="text"
+                  placeholder="Leave a message for us *"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className=" mt-6">
+                <button className="btn btn-primary capitalize">
+                  Send a Message
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
